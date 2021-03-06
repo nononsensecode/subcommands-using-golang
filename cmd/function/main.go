6f29ce6cc97baad9e831c10ebd966afd709f85d8
Kaushik Asokan <kaushikam@gmail.com>
2021-03-06 11:51:54 +0530

    Flag and argument details

--- cmd/function/main.go
+++ cmd/function/main.go
@@ -6,8 +6,20 @@ import (
 )
 
 func main() {
-	flag.Func("usage", "Default message", func(s string) error {
-		fmt.Printf("Testing usage function: %s\n", s)
+	name := "unknown"
+	flag.Func("name", "`name` of the user", func(s string) error {
+		if len(s) < 5 {
+			return fmt.Errorf("Length of name %s is less than 5", s)
+		}
+
+		name = s
 		return nil
 	})
+
+	flag.Parse()
+
+	fmt.Printf("Name: %s\n", name)
+	fmt.Printf("N args: %d\n", flag.NArg())
+	fmt.Printf("N Flags: %d\n", flag.NFlag())
+	fmt.Printf("Args: %s\n", flag.Args())
 }
